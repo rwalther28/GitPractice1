d79462013d052ba2502a2c8047aed9eb71807e5f
Reese <reesemiyowalther@gmail.com>
2025-07-23 08:04:59 -1000

    Initial Commit

---
+++ GitPractice1/GitPractice1App.swift
@@ -0,0 +1,17 @@
+//
+//  GitPractice1App.swift
+//  GitPractice1
+//
+//  Created by rwalther28 on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct GitPractice1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
